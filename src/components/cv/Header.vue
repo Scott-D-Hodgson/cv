536bbb4a27d55fa4668d273c442751a49bfaa65d
Scott D Hodgson <534448@gmail.com>
2024-11-10 10:10:36 -0500

    Add flavour text

---
+++ src/components/cv/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useText } from '../../composables/useText';
+
+const text = useText();
+</script>
+
+<template>
+    <h1 :class="text.standard()">
+        Scott D Hodgson
+    </h1>
+</template>
+
+<style scoped></style>
---
+++ src/components/cv/Section.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useText } from '../../composables/useText';
+
+export interface Props {
+    heading?: string;
+}
+
+const props = defineProps<Props>();
+const text = useText();
+
+</script>
+
+<template>
+    <div class="row mt-3">
+        <div class="col-12" :class="text.standard()">
+            <template v-if="props.heading">
+                <h2>{{ heading }}</h2>
+                <hr>
+            </template>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
