c889a8674417ab0e153c396b46e979748c0d60eb
Scott D Hodgson <534448@gmail.com>
2024-11-10 19:01:22 -0500

    Add scroll to top and bottom

---
+++ src/components/buttons/ScrollToTop.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useText } from '../../composables/useText';
+
+const text = useText();
+
+const scroll = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+</script>
+
+<template>
+    <button 
+        class="btn"
+        :class="text.standard()"
+        title="Scroll to top"
+        @click="scroll">
+        <span class="fad fa-fw fa-up-to-line"></span>
+    </button>
+</template>
+
+<style scoped></style>
