536feae9f55c42cf53b7dce6df498447c38a4ded
Scott D Hodgson <534448@gmail.com>
2024-11-12 19:59:05 -0500

    Remove unused reference

--- src/components/CirriculumVitae.vue
+++ src/components/CirriculumVitae.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import { useDarkMode } from '../composables/useDarkMode';
 import Employment from './cv/Employment.vue';
 import FlavourText from './cv/FlavourText.vue';
 import Header from './cv/Header.vue';
 import Technologies from './cv/Technologies.vue';
-
-const darkMode = useDarkMode();
 </script>
 
 <template>
