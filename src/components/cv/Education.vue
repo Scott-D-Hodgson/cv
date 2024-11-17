8314e864633ac18cc5c8ba3dcbfdead05f095fc9
Scott D Hodgson <534448@gmail.com>
2024-11-17 15:10:03 -0500

    Add Algonquin Conllege to Education

---
+++ src/components/cv/Education.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Section from '../layout/Section.vue';
+import AlgonquinCollege from './education/AlgonquinCollege.vue';
+</script>
+
+<template>
+    <Section heading="Education">
+        <AlgonquinCollege></AlgonquinCollege>
+    </Section>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/cv/education/AlgonquinCollege.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Trainer from '../../layout/Trainer.vue';
+</script>
+
+<template>
+    <Trainer>
+        <template v-slot:provider>Algonquin College</template>
+        <template v-slot:range>2000 - 2001</template>
+        <p>
+            <strong>Diploma:</strong>&nbsp;Computer Programming,
+            Graduated with Honours
+        </p>
+    </Trainer>
+</template>
+
+<style scoped></style>
