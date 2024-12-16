92486b00ba9efa393a0b93d5e01b055a63bde70a
Scott D Hodgson <534448@gmail.com>
2024-12-15 22:55:08 -0500

    Add temporary banner

--- src/components/CirriculumVitae.vue
+++ src/components/CirriculumVitae.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
-import { useDarkMode } from '../composables/useDarkMode';
+import Banner from './cv/Banner.vue';
 import Education from './cv/Education.vue';
 import Employment from './cv/Employment.vue';
 import FlavourText from './cv/FlavourText.vue';
 import Header from './cv/Header.vue';
 import Systems from './cv/Systems.vue';
 import Technologies from './cv/Technologies.vue';
-
-const darkMode = useDarkMode();
 </script>
 
 <template>
     <div>
-        <div class="sticky-top" :class="darkMode.isOn() ? 'bg-dark' : 'bg-light'">
-            <Header></Header>
-        </div>
-        <hr/>
+        <Header></Header>
+        <Banner></Banner>
         <FlavourText></FlavourText>
         <hr/>
         <Technologies></Technologies>
---
+++ src/components/cv/Banner.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="row">
+        <div class="col">
+            <div class="alert alert-warning" role="alert">
+                <p>
+                    In complete transparency, this online <abbr title="Curriculum Vitae">CV</abbr> was intended as a project to be completed over the Christmas break, 2024, to serve as an online footprint in the new year ahead.  Due to an unforeseen opportunity that presented itself, this site was desired prior to the holidays.  As a result, this is presented for the interim as a <abbr title="Work-in-Progress">WIP</abbr> for the remainder of 2024.  Visit again in the near future for a more complete experience.
+                </p>
+                <p>
+                    Current to-do list:
+                    <ul>
+                        <li>Complete term information content.</li>
+                        <li>Expand menu label text responsively on wider screens.</li>
+                        <li>Initialize on dark-mode pattern and fade into colour.</li>
+                        <li>Perform full accessibility evaluation and update.</li>
+                    </ul>
+                </p>
+                <p>
+                    Thank you for your understanding and patience.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
